480bff7f069af98de4bbf9d37b145979d860ea3b
Sean McInTyre <pseanmcintyre@hotmail.com>
2023-03-23 05:44:31 -0400

    Lesson 12 Done

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,7 @@
 const { clear } = require('console');
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 /////////////////////////////////////
 // Blocking, synchronous
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -35,8 +36,22 @@ const http = require('http');
 // SERVER
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  res.end('Hello from the server!');
+  console.log(req.url);
+
+  const pathName = req.url;
+  if (pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else if (pathName === '/') {
+    res.end('This is the HOMEPAGE');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
