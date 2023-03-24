f11355d089d4232302c838d3cc0ee5ef2659d0c0
Sean McInTyre <pseanmcintyre@hotmail.com>
2023-03-24 06:40:01 -0400

    Lesson 15 completed

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,61 +1,58 @@
-'Use Strict'
+
 const { clear } = require('console');
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-/////////////////////////////////////
-// Blocking, synchronous
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log(`File written!`);
-
-// Non-blocking, asynchronous
-// fs.readFile('./txt/start.txt', 'utf-8', (err, fileText1) => {
-//   if (err) return console.log(`ERROR: ${err}`);
-//   fs.readFile(`./txt/${fileText1}.txt`, 'utf-8', (err, fileText2) => {
-//     console.log(fileText2);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, fileText3) => {
-//       console.log(fileText3);
-//       fs.writeFile('./txt/final.txt', `${fileText2}\n${fileText3}`, err => {
-//         if(err) throw err;
-//         console.log ('Your file has been written!');
-//       });
-//     });
-//   });
-// });
-
-
-// fs.writeFile('./txt/startout', (err, data)=>{
-//   if (err) throw err;
-//   console.log(data);
-// });
-// console.log('File 2 written!')
-///////////////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic' );
+  return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-
   const pathName = req.url;
-  if (pathName === '/overview') {
-    res.end('This is the OVERVIEW');
+
+// Overview page
+  if (pathName === '/' || pathName=== '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  
+// Product page  
   } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
-  } else if (pathName === '/') {
-    res.end('This is the HOMEPAGE');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(tempProduct);
+ 
+// API page
   } else if (pathName === '/api') {
-    res.writeHead(200, { 'Content-type': 'application/json'});
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+
+// Page not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world'
-    });
-    res.end('<h1>Page not found!</h1>');
+       });
+     res.end('<h1>Page not found!</h1>');
   }
 });
 
